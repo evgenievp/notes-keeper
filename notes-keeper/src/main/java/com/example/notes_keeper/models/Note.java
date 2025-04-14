166b2c7d6110934e216afb3f3699e31c19227d90
evgenievp <evgenievp@yahoo.com>
2025-04-14 12:15:24 +0300

    note object

---
+++ notes-keeper/src/main/java/com/example/notes_keeper/models/Note.java
@@ -0,0 +1,49 @@
+package com.example.notes_keeper.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public Note() {}
+
+    @PrePersist
+    public void setCreationTimeStamp() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+
+}
